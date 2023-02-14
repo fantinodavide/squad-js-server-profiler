1d8ad60677ae3b3466ec393d46444ac2aaa84f6f
Davide Fantino <80767709+fantinodavide@users.noreply.github.com>
2023-02-15 00:43:18 +0100

    feat: working

--- server-profiler.js
+++ server-profiler.js
@@ -1,5 +1,5 @@
 import DiscordBasePlugin from './discord-base-plugin.js';
-import SocketIOAPI from './socket-io-api.js';
+import TpsLogger from './tps-logger.js';
 import LogParser from '../../core/log-parser/index.js';
 import { MessageAttachment } from "discord.js";
 
@@ -33,19 +33,37 @@ export default class ServerProfiler extends DiscordBasePlugin {
         this.matchProfilerLog = this.matchProfilerLog.bind(this);
         this.roundEnded = this.roundEnded.bind(this);
         this.onTpsDrop = this.onTpsDrop.bind(this);
+        this.csvProfilerEnded = this.csvProfilerEnded.bind(this);
+        this.profilerStart = this.profilerStart.bind(this);
+        this.profilerStop = this.profilerStop.bind(this);
+        this.profilerRestart = this.profilerRestart.bind(this);
+
+        this.TpsLogger = this.server.plugins.find(p => p instanceof TpsLogger);
 
         this.broadcast = this.server.rcon.broadcast;
         this.warn = this.server.rcon.warn;
     }
 
+    async profilerStart() {
+        await this.server.rcon.execute('AdminProfileServerCSV start')
+    }
+    async profilerStop() {
+        await this.server.rcon.execute('AdminProfileServerCSV stop')
+    }
+    async profilerRestart() {
+        this.verbose(1, 'Restarting profiler')
+        await this.profilerStop();
+        setTimeout(this.profilerStart, 2000)
+    }
+
     async mount() {
+        this.verbose(1, 'Mounted.')
         // this.bindListeners();
         // console.log(this.server.logParser.processLine)
-        this.httpServer();
-
-        this.server.on('TICK_RATE', this.tickRateUpdated)
         this.server.on('ROUND_ENDED', this.roundEnded)
         this.server.on('TPS_DROP', this.onTpsDrop)
+        // this.server.on('CSV_PROFILER_ENDED', this.csvProfilerEnded)
+
 
         // setTimeout(this.roundEnded, 2000)
     }
@@ -54,38 +72,36 @@ export default class ServerProfiler extends DiscordBasePlugin {
         this.verbose(1, 'TpsLogger unmounted');
     }
 
-    onTpsDrop(dt) {
+    async onTpsDrop(dt) {
         this.verbose(1, 'Detected TPS Drop', dt)
-        
+        this.server.once('CSV_PROFILER_ENDED', (info) => {
+            this.verbose(1, 'CSV Profiler ENDED', info)
+            this.TpsLogger.tickRates[ dt.id ].profiler_csv_path = info.groups.csv_file_path;
+            // console.log(this.TpsLogger.tickRates[ dt.id ]);
+            this.profilerStart();
+        })
+        await this.profilerStop();
+
     }
 
     async roundEnded(info) {
-        await this.sendDiscordMessage({
-            embed: {
-                title: `TPS Logs`,
-                fields: [
-                    {
-                        name: 'LayerID',
-                        value: this.tickRates[ this.getLatestTpsRecord() ].layer,
-                        inline: false
-                    },
-                ]
-            },
-            timestamp: (new Date()).toISOString()
-        });
-        await this.sendDiscordMessage({
-            files: [
-                new MessageAttachment(Buffer.from(JSON.stringify(this.tickRates, null, 2)), 'TPS_History.json')
-            ]
-        })
-        this.tickRates = [];
+        await this.profilerStop();
+    }
+
+    csvProfilerEnded(dt) {
+        this.verbose(1, 'CSV Profiler ENDED', dt)
+        this.TpsLogger.tickRates[ dt.id ].profiler_csv_path = dt.groups.csv_file_path;
+        console.log(this.TpsLogger.tickRates[ dt.id ]);
+        this.profilerStart();
     }
 
     matchProfilerLog(line) {
         const regex = /LogCsvProfiler\: Display\: Capture (?<state>\w+)(. CSV ID: (?<csv_id>\w+))?(. Writing CSV to file : (?<csv_file_path>.+))?/
         const match = line.match(regex);
         if (match) {
-            this.server.emit(`CSV_PROFILER_${match.groups.state.toUpperCase()}`, match)
+            const event = `CSV_PROFILER_${match.groups.state.toUpperCase()}`;
+            this.server.emit(event, match)
+            this.verbose(1, 'Emitting event', event)
         }
     }
 }
